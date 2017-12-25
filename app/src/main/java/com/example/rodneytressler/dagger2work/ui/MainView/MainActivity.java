553a8646ce6db1d62036d65ca885c328a4b9e0e4
Rodney Tressler <rodneytressler@rodneys-mbp.lan>
2017-12-24 22:25:47 -0500

    [INIT] first commit.

---
+++ app/src/main/java/com/example/rodneytressler/dagger2work/ui/MainView/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rodneytressler.dagger2work.ui.MainView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rodneytressler.dagger2work.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
